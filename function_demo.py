548e8ba089cd7e5a294e1a7fdf6c0afe636e8295
jishan <jishanalam.qa@gmail.com>
2023-01-08 20:06:23 +0530

    Add new function

---
+++ function_demo.py
@@ -0,0 +1,10 @@
+def add_numbers(n1, n2):
+    sum_result = n1+n2
+    return sum_result
+
+
+number1 = int(input("Enter number1:"))
+number2 = int(input("Enter number2:"))
+
+sum_result = add_numbers(number1, number2)
+print("Sum of the numbers is:", sum_result)
