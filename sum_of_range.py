6286389093c18473fe4bcfd51064d69d9305c43c
jishan <jishanalam.qa@gmail.com>
2023-01-08 20:41:40 +0530

    Adding new function

---
+++ sum_of_range.py
@@ -0,0 +1,6 @@
+# number = int(input("Enter a number: "))
+total = 0
+for number in range(1, 101):
+    total = total + number
+    # print(number, "+", count, "=", sum_of_list)
+    print("Sum of the list is: ", total)
